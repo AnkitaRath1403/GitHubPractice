1f6b160b7f38a6ca84ebd866ef24682169d890cb
siddh <ankitarath1403@gmail.com>
2023-08-03 20:43:05 +0530

    New Package committed

---
+++ src/test/java/Demo2/Practice.java
@@ -0,0 +1,11 @@
+package Demo2;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+
+	@Test
+	public void test() {
+		System.out.println("My life is going to be vewry happy and perfect");
+	}
+}
