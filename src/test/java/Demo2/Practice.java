6ea410f177b45b87a633a1cb45202350ef5dee96
siddh <ankitarath1403@gmail.com>
2023-08-04 19:57:37 +0530

    New code committed

--- src/test/java/Demo2/Practice.java
+++ src/test/java/Demo2/Practice.java
@@ -7,5 +7,6 @@ public class Practice {
 	@Test
 	public void test() {
 		System.out.println("My life is going to be vewry happy and perfect");
+		System.out.println("I am going to be very happy and tension free ");
 	}
 }
